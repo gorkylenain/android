341f924cd019971c4c366b99bf2faa9a7f35a579
gorkylenain <gorkylenain@gmail.com>
2020-04-12 12:01:42 +0200

    Premier envoi

--- app/src/main/java/com/oc/rss/MainActivity.java
+++ app/src/main/java/com/oc/rss/MainActivity.java
@@ -1,7 +1,11 @@
 package com.oc.rss;
 
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button myButton = (Button) findViewById(R.id.button1);
+        myButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.e("OC_RSS", "ça marche !!!");
+            }
+        });
     }
 }
